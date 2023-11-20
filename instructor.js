431711bb53c236d8ca69646706ab9bc38dae5518
Emanuel Manotas <emanuelmanotas2002@gmail.com>
2023-11-20 10:30:38 -0500

    estilos instructor 4

--- instructor.js
+++ instructor.js
@@ -1,12 +1,51 @@
-// abrir ventana de historial
+// abrir y cerrar ventana de historial
 const ventana = document.querySelector('#ventana');
-const btnVentana = document.querySelector("#btn-ventana");
-const btncerrar = document.querySelector("#btn-cerrar") 
-btnVentana.addEventListener("click", ()=>{
-    ventana.showModal();
+const btnVentana = document.querySelectorAll(".btn-ventana");
+const btncerrar = document.querySelectorAll(".btn-cerrar") 
 
-    btncerrar.addEventListener("click", ()=>{
-        ventana.close()
+
+
+btnVentana.forEach((btn)=>{
+    btn.addEventListener("click", ()=>{
+        ventana.showModal();
+    
+    });
+    
+    btncerrar.forEach((btn)=>{
+        btn.addEventListener("click", ()=>{
+            ventana.close()
+        })
+        
     })
-});
+})
+
+
+let dias = document.querySelectorAll(".day")
+
+let principal = document.querySelector(".principal")
+
+let historial = document.querySelector(".historial")
+
+dias.forEach((dia)=>{
+    dia.addEventListener("click", ()=>{
+        principal.style.display="none" 
+        historial.style.display="flex" 
+        
+        setTimeout(()=>{ 
+            historial.style.opacity=1
+
+        }, 50)
+        ventana.close()
+    })  
+})
+
+let btnDiaActual = document.querySelector(".btn-dia-actual")
+btnDiaActual.addEventListener("click", ()=>{ 
+    historial.style.display="none" 
+    principal.style.display="flex" 
+    setTimeout(()=>{ 
+        principal.style.opacity=1
+
+    }, 50)
+})
 
