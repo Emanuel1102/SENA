31549ae04af30a05a4caac2dd6f71d459371262b
Emanuel Manotas <emanuelmanotas2002@gmail.com>
2023-11-22 11:28:20 -0500

    funcionalidad de menús desplegables

--- administrativo/administrativo.js
+++ administrativo/administrativo.js
@@ -1,7 +1,8 @@
+// abrir y cerrar ventanas
 let btnsVentana = document.querySelectorAll(".btn-ventana")
 
 let btnsCerrar = document.querySelectorAll(".btn-cerrar")
-
+// ABRIR
 btnsVentana.forEach((boton)=>{
     boton.addEventListener("click", ()=>{
         boton.nextElementSibling.showModal()
@@ -9,7 +10,7 @@ btnsVentana.forEach((boton)=>{
     })
 
 })
-
+// CERRAR
 btnsCerrar.forEach((boton_cerrar)=>{
     boton_cerrar.addEventListener("click", ()=>{
         boton_cerrar.closest(".ventana").close()
@@ -17,3 +18,19 @@ btnsCerrar.forEach((boton_cerrar)=>{
     })
 
 })
+
+
+let pisos = [...document.querySelectorAll(".piso")]
+
+pisos.forEach((pisoClick)=>{
+    pisoClick.addEventListener("click", ()=>{
+        pisos.forEach((piso)=>{
+            if (piso !== pisoClick && piso.hasAttribute("open")) {
+                piso.removeAttribute("open")
+            }
+
+        })
+    })
+
+})
+
