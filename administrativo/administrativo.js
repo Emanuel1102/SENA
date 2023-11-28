640d3bf9a073dccc230ba857692f4b7a755481ad
Emanuel Manotas <emanuelmanotas2002@gmail.com>
2023-11-28 05:45:51 +0100

    estilos de interfaz de usuarios 1

--- administrativo/administrativo.js
+++ administrativo/administrativo.js
@@ -34,3 +34,18 @@ pisos.forEach((pisoClick)=>{
 
 })
 
+
+
+let opciones = document.querySelectorAll(".opcion")
+
+opciones.forEach((opcionClick)=>{
+    opcionClick.addEventListener("click", ()=>{
+        opcionClick.classList.add("seleccion")
+
+        opciones.forEach((opcion)=>{
+            if (opcionClick !== opcion && opcion.classList.contains("seleccion")){
+                opcion.classList.remove("seleccion")
+            }
+        })
+    })
+})
